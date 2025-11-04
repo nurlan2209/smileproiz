e93acd4fc8d262cf27efe712a484cf63f43d7cc0
nurlan2209 <nkuanyshuly2020@gmail.com>
2025-11-04 05:37:37 +0500

    second

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
   // Для iOS и Web
-  static const String baseUrl = 'http://localhost:8080/api/users';
+  static const String baseUrl = 'http://smileproiz-production.up.railway.app/api/users';
 
   // Проверка статуса авторизации
   Future<bool> isLoggedIn() async {
